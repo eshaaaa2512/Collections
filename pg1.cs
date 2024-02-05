5662bf83e7420bb9eadb797633628222c8b7bc6e
Esha <eshadeswal@gmail.com>
2024-02-05 09:43:16 +0530

    program for queue

--- pg1.cs
+++ pg1.cs
@@ -1,27 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
-namespace List
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        // Creating a Queue
+        Queue myQueue = new Queue();
+
+        // Enqueue elements
+        myQueue.Enqueue("Element 1");
+        myQueue.Enqueue("Element 2");
+        myQueue.Enqueue("Element 3");
+
+        // Displaying the elements in the Queue
+        DisplayQueue(myQueue);
+
+        // Peek at the front element
+        Console.WriteLine("Front element: " + myQueue.Peek());
+
+        // Dequeue elements
+        while (myQueue.Count > 0)
         {
-            List<int> arr;
-            arr = new List<int>();
-
-            arr.Add(1);
-            arr.Add(2);
-            arr.Add(3);
-            arr.Add(4);
-            arr.Add(5);
-
-            Console.WriteLine(arr.Count);
-            Console.WriteLine(arr.Contains(1));
+            Console.WriteLine("Dequeue: " + myQueue.Dequeue());
         }
 
+        // Check if the Queue is empty
+        Console.WriteLine("Is the Queue empty? " + (myQueue.Count == 0));
+    }
+
+    static void DisplayQueue(Queue queue)
+    {
+        Console.WriteLine("Elements in the Queue:");
+        foreach (var item in queue)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine();
     }
 }
