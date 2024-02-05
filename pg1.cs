2ae07a88777055cf10d020b483f3ac5c1ffb693d
Esha <eshadeswal@gmail.com>
2024-02-05 08:51:50 +0530

    program for stack

--- pg1.cs
+++ pg1.cs
@@ -1,27 +1,41 @@
 ﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace List
+namespace Collection_Framework
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<int> arr;
-            arr = new List<int>();
 
-            arr.Add(1);
-            arr.Add(2);
-            arr.Add(3);
-            arr.Add(4);
-            arr.Add(5);
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            Console.WriteLine(stack.Count());
 
-            Console.WriteLine(arr.Count);
-            Console.WriteLine(arr.Contains(1));
+            Stack stack1 = new Stack();
+            stack1.Push(1);
+            stack1.Push(2);
+            stack1.Pop();
+            stack1.Push(4);
+            stack1.Push(5);
+            Console.WriteLine(stack1.Count);
+            Console.WriteLine(stack1.Peek());
+
+
+            //Various operations for stack are 
+            //Push()
+            //Pop()
+            //Peek
+
         }
-
     }
 }
