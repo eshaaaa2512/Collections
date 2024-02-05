6f19c9944ff5aabb9a74e74fd0f2c60c755f49dc
Esha <eshadeswal@gmail.com>
2024-02-05 09:46:05 +0530

    program for hashset

--- pg1.cs
+++ pg1.cs
@@ -1,27 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace List
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        // Creating a HashSet
+        HashSet<int> myHashSet = new HashSet<int>();
+
+        // Adding elements to the HashSet
+        myHashSet.Add(1);
+        myHashSet.Add(2);
+        myHashSet.Add(3);
+
+        // Displaying the elements in the HashSet
+        DisplayHashSet(myHashSet);
+
+        // Checking if an element is present
+        int searchElement = 2;
+        Console.WriteLine($"Is {searchElement} present in the HashSet? {myHashSet.Contains(searchElement)}");
+
+        // Removing an element
+        myHashSet.Remove(2);
+
+        // Displaying the updated HashSet
+        Console.WriteLine("\nHashSet after removing element 2:");
+        DisplayHashSet(myHashSet);
+
+        // Clearing the HashSet
+        myHashSet.Clear();
+
+        // Checking if the HashSet is empty
+        Console.WriteLine("\nIs the HashSet empty? " + (myHashSet.Count == 0));
+    }
+
+    static void DisplayHashSet(HashSet<int> hashSet)
+    {
+        Console.WriteLine("Elements in the HashSet:");
+        foreach (var item in hashSet)
         {
-            List<int> arr;
-            arr = new List<int>();
-
-            arr.Add(1);
-            arr.Add(2);
-            arr.Add(3);
-            arr.Add(4);
-            arr.Add(5);
-
-            Console.WriteLine(arr.Count);
-            Console.WriteLine(arr.Contains(1));
+            Console.Write(item + " ");
         }
-
+        Console.WriteLine();
     }
 }
