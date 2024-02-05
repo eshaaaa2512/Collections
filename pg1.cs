2bb7078b0eef99e496b68cf3f60074ff2f928016
Esha <eshadeswal@gmail.com>
2024-02-05 08:48:05 +0530

    List

---
+++ pg1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr;
+            arr = new List<int>();
+
+            arr.Add(1);
+            arr.Add(2);
+            arr.Add(3);
+            arr.Add(4);
+            arr.Add(5);
+
+            Console.WriteLine(arr.Count);
+            Console.WriteLine(arr.Contains(1));
+        }
+
+    }
+}
